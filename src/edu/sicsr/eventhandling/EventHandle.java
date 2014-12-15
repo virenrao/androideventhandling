3bc3ca5f593d52cd493e6d42cb71fb4a0fd0d4bb
virenrao <virenrao@yahoo.com>
2014-12-15 09:45:33 +0530

    added button and button click action

--- src/edu/sicsr/eventhandling/EventHandle.java
+++ src/edu/sicsr/eventhandling/EventHandle.java
@@ -1,23 +1,48 @@
 package edu.sicsr.eventhandling;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class EventHandle extends Activity {
-
+	private Button btnClick;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_handle);
+       
+        btnClick = (Button) findViewById(R.id.btnClick);
+        OnClickListener listener = new OnClickListener() {
+        	
+        	
+        	
+        	
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				btnClick.setText("button was clicked!");
+				
+				
+				
+			}
+		};
+        btnClick.setOnClickListener(listener);
     }
 
+    
 
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.event_handle, menu);
         return true;
+        
+       
     }
     
 }
